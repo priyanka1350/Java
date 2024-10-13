2cff37a81720b720f1c7c74a652723fc540d690a
priyanka1350 <priyankamahesh1350.com>
2024-10-14 00:57:51 +0530

    Objects

---
+++ ObjectsSession/DessertDetails.java
@@ -0,0 +1,125 @@
+public class DessertDetails {
+    public static void main(String[] args) {
+
+        Dessert dessert = new Dessert();
+        dessert.dessertID = 1;
+        dessert.dessertName = "Chocolate Cake";
+        dessert.price = 350;
+        dessert.quantityAvailable = 20;
+        dessert.isVegan = false;
+        dessert.calories = 500;
+
+        System.out.println("Dessert ID: " + dessert.dessertID);
+        System.out.println("Dessert Name: " + dessert.dessertName);
+        System.out.println("Price: " + dessert.price);
+        System.out.println("Quantity Available: " + dessert.quantityAvailable);
+        System.out.println("Is Vegan: " + dessert.isVegan);
+        System.out.println("Calories: " + dessert.calories);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Dessert dessert01 = new Dessert();
+        dessert01.dessertID = 2;
+        dessert01.dessertName = "Strawberry Cheesecake";
+        dessert01.price = 400;
+        dessert01.quantityAvailable = 15;
+        dessert01.isVegan = false;
+        dessert01.calories = 450;
+
+        System.out.println("Dessert ID: " + dessert01.dessertID);
+        System.out.println("Dessert Name: " + dessert01.dessertName);
+        System.out.println("Price: " + dessert01.price);
+        System.out.println("Quantity Available: " + dessert01.quantityAvailable);
+        System.out.println("Is Vegan: " + dessert01.isVegan);
+        System.out.println("Calories: " + dessert01.calories);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Dessert dessert02 = new Dessert();
+        dessert02.dessertID = 3;
+        dessert02.dessertName = "Vegan Brownie";
+        dessert02.price = 300;
+        dessert02.quantityAvailable = 10;
+        dessert02.isVegan = true;
+        dessert02.calories = 350;
+
+        System.out.println("Dessert ID: " + dessert02.dessertID);
+        System.out.println("Dessert Name: " + dessert02.dessertName);
+        System.out.println("Price: " + dessert02.price);
+        System.out.println("Quantity Available: " + dessert02.quantityAvailable);
+        System.out.println("Is Vegan: " + dessert02.isVegan);
+        System.out.println("Calories: " + dessert02.calories);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Dessert dessert03 = new Dessert();
+        dessert03.dessertID = 4;
+        dessert03.dessertName = "Mango Ice Cream";
+        dessert03.price = 200;
+        dessert03.quantityAvailable = 25;
+        dessert03.isVegan = false;
+        dessert03.calories = 250;
+
+        System.out.println("Dessert ID: " + dessert03.dessertID);
+        System.out.println("Dessert Name: " + dessert03.dessertName);
+        System.out.println("Price: " + dessert03.price);
+        System.out.println("Quantity Available: " + dessert03.quantityAvailable);
+        System.out.println("Is Vegan: " + dessert03.isVegan);
+        System.out.println("Calories: " + dessert03.calories);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Dessert dessert04 = new Dessert();
+        dessert04.dessertID = 5;
+        dessert04.dessertName = "Apple Pie";
+        dessert04.price = 450;
+        dessert04.quantityAvailable = 30;
+        dessert04.isVegan = false;
+        dessert04.calories = 400;
+
+        System.out.println("Dessert ID: " + dessert04.dessertID);
+        System.out.println("Dessert Name: " + dessert04.dessertName);
+        System.out.println("Price: " + dessert04.price);
+        System.out.println("Quantity Available: " + dessert04.quantityAvailable);
+        System.out.println("Is Vegan: " + dessert04.isVegan);
+        System.out.println("Calories: " + dessert04.calories);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Dessert dessert05 = new Dessert();
+        dessert05.dessertID = 6;
+        dessert05.dessertName = "Pineapple Pie";
+        dessert05.price = 300;
+        dessert05.quantityAvailable = 20;
+        dessert05.isVegan = false;
+        dessert05.calories = 450;
+
+        System.out.println("Dessert ID: " + dessert05.dessertID);
+        System.out.println("Dessert Name: " + dessert05.dessertName);
+        System.out.println("Price: " + dessert05.price);
+        System.out.println("Quantity Available: " + dessert05.quantityAvailable);
+        System.out.println("Is Vegan: " + dessert05.isVegan);
+        System.out.println("Calories: " + dessert05.calories);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Dessert dessert06 = new Dessert();
+        dessert06.dessertID = 7;
+        dessert06.dessertName = "Lemon Tart";
+        dessert06.price = 350;
+        dessert06.quantityAvailable = 10;
+        dessert06.isVegan = false;
+        dessert06.calories = 300;
+
+        System.out.println("Dessert ID: " + dessert06.dessertID);
+        System.out.println("Dessert Name: " + dessert06.dessertName);
+        System.out.println("Price: " + dessert06.price);
+        System.out.println("Quantity Available: " + dessert06.quantityAvailable);
+        System.out.println("Is Vegan: " + dessert06.isVegan);
+        System.out.println("Calories: " + dessert06.calories);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    }
+}
+
